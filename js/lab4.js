0eab623fe4d27d8c1fdeb496af942da2c8fe89d0
joelmu <joel.muittari@gmail.com>
2018-02-14 20:15:30 +0200

    coordinates still not working

--- js/lab4.js
+++ js/lab4.js
@@ -1,47 +1,49 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var preview = document.getElementById("preview");
+var prew = preview.getContext("2d");
 var mouseIsDown = false;
-var rect = canvas.getBoundingClientRect();
+var lineThickness = 1;
 var cX, cY, eX, eY;
 
-canvas.onmouseup = function mouseUp(e) {
+preview.onmouseup = function mouseUp(e) {
   mouseIsDown = false;
 
-  eX = e.clientX - rect.left;
-  eY = e.clientY - rect.top;
+  // eX = e.clientX - rect.left;
+  // eY = e.clientY - rect.top;
 
-  ctx.strokeStyle = "#267bdf";
+  ctx.strokeStyle = "#000";
   ctx.beginPath();
   ctx.moveTo(cX, cY);
   ctx.lineTo(eX, eY);
   ctx.stroke();
 
-  // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  // prew.clearRect(0, 0, prew.canvas.width, prew.canvas.height);
 };
 
-canvas.onmousedown = function mouseDown(e) {
-  cX = e.clientX - rect.left;
-  cY = e.clientY - rect.top;
+preview.onmousedown = function mouseDown(e) {
+  cX = e.clientX - canvas.offsetLeft;
+  cY = e.clientY - canvas.offsetTop;
   mouseIsDown = true;
 };
 
-// canvas.onmousemove = function mouseXY(e) {
-//   if (mouseIsDown) {
-//     eX = e.clientX - rect.left;
-//     eY = e.clientY - rect.top;
-//
-//     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-//
-//     ctx.beginPath();
-//     ctx.moveTo(cX, cY);
-//     ctx.lineTo(eX, eY);
-//     ctx.stroke();
-//
-//   }
-// };
+preview.onmousemove = function mouseXY(e) {
+  if (mouseIsDown) {
+    eX = e.clientX - canvas.offsetLeft;
+    eY = e.clientY - canvas.offsetTop;
 
-canvas.onmouseclick = function mouseclick(e) {
-  cX = e.clientX - rect.left;
-  cY = e.clientY - rect.top;
+    prew.clearRect(0, 0, prew.canvas.width, prew.canvas.height);
+
+    prew.beginPath();
+    prew.moveTo(cX, cY);
+    prew.lineTo(eX, eY);
+    prew.stroke();
+
+  }
+};
+
+preview.onmouseclick = function mouseclick(e) {
+  cX = e.clientX - canvas.offsetLeft;
+  cY = e.clientY - canvas.offsetTop;
   mouseIsDown = true;
 }
